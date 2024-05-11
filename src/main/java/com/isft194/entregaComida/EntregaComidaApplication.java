d56c0787aaecc907c0462fad14fc23f40166bbe4
nahuel1558 <nahuelzubiri@hotmail.es>
2024-05-11 16:11:46 -0300

    Proyecto inicial

---
+++ src/main/java/com/isft194/entregaComida/EntregaComidaApplication.java
@@ -0,0 +1,13 @@
+package com.isft194.entregaComida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaComidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaComidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isft194/entregaComida/EntregaComidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isft194.entregaComida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaComidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
